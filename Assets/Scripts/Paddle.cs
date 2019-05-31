777f216a48157afd68ea5e2b7d4f9880018967be
akasharkxx <akash.jaiswar41@gmail.com>
2019-06-01 01:05:13 +0530

    Added ball.cs script
    added ball.cs script
    made starting conditions of the game
    updated paddle.cs

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //a variable of vector3 type for paddle position as transform.position reuturn of type vector3
+        Vector3 paddlePos = new Vector3(0.5f, this.transform.position.y, 0f); //for keeping y position
+
+        float mousePosInBlocks = Input.mousePosition.x/ Screen.width * 16; //Game space is 16 world units wide
+        //print (mousePosInBlocks);
+
+        paddlePos.x = Mathf.Clamp(mousePosInBlocks, 0.5f, 15.5f);
+        this.transform.position = paddlePos; //this is not neccesary   
+    }
+}
---
+++ Assets/Scripts/ball.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour
+{
+    public Paddle paddle;
+    private bool hasStarted = false;
+    private Vector3 paddleToBallVector;
+    // Start is called before the first frame update
+    void Start() {
+        //getting the current tranforms of both ball and paddle
+        paddleToBallVector = this.transform.position - paddle.transform.position;
+    }//end of method start
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if(!hasStarted){
+            //to make ball stick to the paddle at the start of game
+            this.transform.position = paddle.transform.position + paddleToBallVector;
+            
+            //to get mouse input and launch ball
+            if(Input.GetMouseButtonDown (0)){
+                print ("mouse clicked, launching ball");
+                //make hasStarted true as this should not run after launching the ball
+                hasStarted = true;
+                //we need to give the ball some velocity
+                this.GetComponent<Rigidbody2D>().velocity = new Vector2(2f, 10f);
+            }//end of if
+        }//end of if
+    }//end of update method
+}
