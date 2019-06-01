4ffe1a323e400a0388a4c371945c60611c248ba3
akasharkxx <akash.jaiswar41@gmail.com>
2019-06-02 01:14:04 +0530

    Added new Function in LevelManager.cs and Bricks.cs
    Added LoadNextLevel() in LevelManager.cs in which uses index of levels
    Added new Function SimulateWin() in Bricks.cs to change levels and in the end win
    The win conditions are not set till now

--- Assets/Scripts/Bricks.cs
+++ Assets/Scripts/Bricks.cs
@@ -5,19 +5,23 @@ using UnityEngine;
 public class Bricks : MonoBehaviour
 {
     public int maxHits;
+    private LevelManager levelmanager;
     private int timesHit;
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start(){
+        levelmanager = GameObject.FindObjectOfType<LevelManager>();
         timesHit = 0;
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update(){
         
     }
     void OnCollisionEnter2D (Collision2D collision){
         timesHit++;
+        SimulateWin();
+    }// TODO remove this function once we can win!!!
+    void SimulateWin() {
+        levelmanager.LoadNextLevel();
     }
 }
